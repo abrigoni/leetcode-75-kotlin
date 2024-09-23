f0ba727ccd033cefad7b101b363f5fc42fc942e0
abrigoni <brian.rigoni1@gmail.com>
2024-09-24 01:48:18 +0200

    contains_duplicate

---
+++ src/main/kotlin/arrays_hashing/contains_duplicate.kt
@@ -0,0 +1,14 @@
+package arrays_hashing
+
+class ContainsDuplicateSolution {
+    fun containsDuplicate(nums: IntArray): Boolean {
+        val set = mutableSetOf<Int>();
+        for (num in nums) {
+            if (set.contains(num)) {
+                return true
+            }
+            set.add(num)
+        }
+        return false
+    }
+}
---
+++ src/test/kotlin/arrays_hashing/contains_duplicate_test.kt
@@ -0,0 +1,26 @@
+package arrays_hashing
+
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class ContainsDuplicateTest {
+    @Test
+    fun test_duplicate_true() {
+        val solution = ContainsDuplicateSolution()
+        val nums = intArrayOf(1, 2, 4, 1)
+        assertTrue(solution.containsDuplicate(nums))
+    }
+    @Test
+    fun test_no_duplicate_false() {
+        val solution = ContainsDuplicateSolution()
+        val nums = intArrayOf(1, 2, 3)
+        assertFalse(solution.containsDuplicate(nums))
+    }
+    @Test
+    fun test_empty_false() {
+        val solution = ContainsDuplicateSolution()
+        val nums = intArrayOf()
+        assertFalse(solution.containsDuplicate(nums))
+    }
+}

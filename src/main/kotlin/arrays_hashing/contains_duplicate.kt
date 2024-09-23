97e98d4767dcf5c2db763266a6ef274d525ca607
abrigoni <brian.rigoni1@gmail.com>
2024-09-24 01:48:43 +0200

    remove semicolon on contains_duplicate.kt

--- src/main/kotlin/arrays_hashing/contains_duplicate.kt
+++ src/main/kotlin/arrays_hashing/contains_duplicate.kt
@@ -2,7 +2,7 @@ package arrays_hashing
 
 class ContainsDuplicateSolution {
     fun containsDuplicate(nums: IntArray): Boolean {
-        val set = mutableSetOf<Int>();
+        val set = mutableSetOf<Int>()
         for (num in nums) {
             if (set.contains(num)) {
                 return true

4978e7e9be0ed5db47b1d72b8dddc5d62330dc6c
abrigoni <brian.rigoni1@gmail.com>
2024-09-24 19:36:47 +0200

    refactor contains_duplicate.kt

--- src/main/kotlin/arrays_hashing/contains_duplicate.kt
+++ src/main/kotlin/arrays_hashing/contains_duplicate.kt
@@ -4,10 +4,10 @@ class ContainsDuplicateSolution {
     fun containsDuplicate(nums: IntArray): Boolean {
         val set = mutableSetOf<Int>()
         for (num in nums) {
-            if (set.contains(num)) {
+            // add returns false if element already exists in set
+            if (!set.add(num)) {
                 return true
             }
-            set.add(num)
         }
         return false
     }
